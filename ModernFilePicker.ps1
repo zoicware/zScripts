680de04a04b9c83edd1889c27705c7916a9b531d
zoicware <118035521+zoicware@users.noreply.github.com>
2025-01-30 17:26:46 -0800

    Modern File Picker in PowerShell

---
+++ ModernFilePicker.ps1
@@ -0,0 +1,61 @@
+function Show-ModernFilePicker {
+    param(
+        [ValidateSet('Folder', 'File')]
+        $Mode,
+        [string]$fileType
+
+    )
+
+    if ($Mode -eq 'Folder') {
+        $Title = 'Select Folder'
+        $modeOption = $false
+        $Filter = "Folders|`n"
+    }
+    else {
+        $Title = 'Select File'
+        $modeOption = $true
+        if ($fileType) {
+            $Filter = "$fileType Files (*.$fileType) | *.$fileType|All files (*.*)|*.*"
+        }
+        else {
+            $Filter = 'All Files (*.*)|*.*'
+        }
+    }
+    #modern file dialog
+    #modified code from: https://gist.github.com/IMJLA/1d570aa2bb5c30215c222e7a5e5078fd
+    $AssemblyFullName = 'System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089'
+    $Assembly = [System.Reflection.Assembly]::Load($AssemblyFullName)
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.AddExtension = $modeOption
+    $OpenFileDialog.CheckFileExists = $modeOption
+    $OpenFileDialog.DereferenceLinks = $true
+    $OpenFileDialog.Filter = $Filter
+    $OpenFileDialog.Multiselect = $false
+    $OpenFileDialog.Title = $Title
+    $OpenFileDialog.InitialDirectory = [Environment]::GetFolderPath('Desktop')
+
+    $OpenFileDialogType = $OpenFileDialog.GetType()
+    $FileDialogInterfaceType = $Assembly.GetType('System.Windows.Forms.FileDialogNative+IFileDialog')
+    $IFileDialog = $OpenFileDialogType.GetMethod('CreateVistaDialog', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($OpenFileDialog, $null)
+    $null = $OpenFileDialogType.GetMethod('OnBeforeVistaDialog', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($OpenFileDialog, $IFileDialog)
+    if ($Mode -eq 'Folder') {
+        [uint32]$PickFoldersOption = $Assembly.GetType('System.Windows.Forms.FileDialogNative+FOS').GetField('FOS_PICKFOLDERS').GetValue($null)
+        $FolderOptions = $OpenFileDialogType.GetMethod('get_Options', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($OpenFileDialog, $null) -bor $PickFoldersOption
+        $null = $FileDialogInterfaceType.GetMethod('SetOptions', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($IFileDialog, $FolderOptions)
+    }
+  
+  
+
+    $VistaDialogEvent = [System.Activator]::CreateInstance($AssemblyFullName, 'System.Windows.Forms.FileDialog+VistaDialogEvents', $false, 0, $null, $OpenFileDialog, $null, $null).Unwrap()
+    [uint32]$AdviceCookie = 0
+    $AdvisoryParameters = @($VistaDialogEvent, $AdviceCookie)
+    $AdviseResult = $FileDialogInterfaceType.GetMethod('Advise', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($IFileDialog, $AdvisoryParameters)
+    $AdviceCookie = $AdvisoryParameters[1]
+    $Result = $FileDialogInterfaceType.GetMethod('Show', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($IFileDialog, [System.IntPtr]::Zero)
+    $null = $FileDialogInterfaceType.GetMethod('Unadvise', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($IFileDialog, $AdviceCookie)
+    if ($Result -eq [System.Windows.Forms.DialogResult]::OK) {
+        $FileDialogInterfaceType.GetMethod('GetResult', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($IFileDialog, $null)
+    }
+
+    return $OpenFileDialog.FileName
+}
