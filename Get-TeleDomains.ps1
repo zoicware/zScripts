27fd8a9ff2f9c4b93a3d7a56314e2f3b3b1c02f5
zoicware <118035521+zoicware@users.noreply.github.com>
2025-06-25 15:18:47 -0400

    Add files via upload

---
+++ Get-TeleDomains.ps1
@@ -0,0 +1,13 @@
+﻿$jsonContent = "$env:ProgramData\Microsoft\Diagnosis\DownloadedSettings\utc.app.json"
+if(Test-Path $jsonContent){
+    $ConvertedContent = Get-Content $jsonContent -Raw | ConvertFrom-Json
+    $lines = $ConvertedContent.settings -split ':' | Where-Object {$_ -match '\/\/[a-zA-Z0-9.-]+\.data\.microsoft\.com\S*'}
+    foreach($line in $lines){
+        if($line -notlike "*; UTC" -and $line -notlike "*settings*"){
+            $cleanLine = ($line -replace '//' ,'' -replace '/collect/v1|https' ,'' -replace '/OneCollector/1.0|https' ,'' -replace '/|' ,'').TrimEnd('|')
+            Write-Host $cleanLine
+        }
+
+    }
+
+}
