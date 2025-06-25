3e7a824e034c86537c47bebf1ddc769d7d71feb0
zoicware <118035521+zoicware@users.noreply.github.com>
2025-06-25 15:28:05 -0400

    Update Get-TeleDomains.ps1

--- Get-TeleDomains.ps1
+++ Get-TeleDomains.ps1
@@ -1,13 +1,16 @@
-﻿$jsonContent = "$env:ProgramData\Microsoft\Diagnosis\DownloadedSettings\utc.app.json"
+$jsonContent = "$env:ProgramData\Microsoft\Diagnosis\DownloadedSettings\utc.app.json"
+$domains = @()
 if(Test-Path $jsonContent){
     $ConvertedContent = Get-Content $jsonContent -Raw | ConvertFrom-Json
     $lines = $ConvertedContent.settings -split ':' | Where-Object {$_ -match '\/\/[a-zA-Z0-9.-]+\.data\.microsoft\.com\S*'}
     foreach($line in $lines){
         if($line -notlike "*; UTC" -and $line -notlike "*settings*"){
             $cleanLine = ($line -replace '//' ,'' -replace '/collect/v1|https' ,'' -replace '/OneCollector/1.0|https' ,'' -replace '/|' ,'').TrimEnd('|')
-            Write-Host $cleanLine
+            $domains += $cleanLine
         }
 
     }
+    $uDomains = $domains | Sort-Object -Unique
+    Write-Host $uDomains
 
 }
