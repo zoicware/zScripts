e506e0d64f017881ec5fbebfe8503a11e92d5178
zoicware <118035521+zoicware@users.noreply.github.com>
2025-06-26 11:24:57 -0400

    unhide all cbs packages from dism

---
+++ UnhidePackages.ps1
@@ -0,0 +1,9 @@
+﻿#unhides all packages from dism /online /get-packages
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\Packages"
+
+Get-ChildItem $regPath | ForEach-Object {
+   $value = Get-ItemPropertyValue "registry::$($_.Name)" -Name Visibility
+   if($value -eq 2){
+       Set-ItemProperty "registry::$($_.Name)" -Name Visibility -Value 1 -Force
+   }
+}
