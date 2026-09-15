3673be207341a04c3368970ffc392e0a7655b7c8
zoicware <118035521+zoicware@users.noreply.github.com>
2026-09-15 11:49:15 -0400

    usb per device saving

--- DeviceManagerPowerSaving.ps1
+++ DeviceManagerPowerSaving.ps1
@@ -19,3 +19,32 @@ foreach ($line in $devices) {
 }
 
 #powercfg -devicequery wake_programmable
+
+$possibleValues = @(
+    [pscustomobject]@{Name = 'AllowIdleIrpInD3'; Value = 0; Type = 'DWord' }
+    [pscustomobject]@{Name = 'SelectiveSuspendEnabled'; Value = 0; Type = 'Binary' }
+    [pscustomobject]@{Name = 'DeviceIdleEnabled'; Value = 0; Type = 'DWord' }
+    [pscustomobject]@{Name = 'SelectiveSuspendOn'; Value = 0; Type = 'DWord' }
+    [pscustomobject]@{Name = 'DefaultIdleState'; Value = 0; Type = 'DWord' } #if this doesnt exist its the same as val = 0
+    [pscustomobject]@{Name = 'DeviceIdleIgnoreWakeEnable'; Value = 0; Type = 'DWord' }
+    [pscustomobject]@{Name = 'ForceSelectiveSuspend'; Value = 0; Type = 'DWord' } #only for bluetooth devices
+    [pscustomobject]@{Name = 'IdleUsbSelectiveSuspendPolicy'; Value = $null; Type = 'DEL' } #del if exists https://learn.microsoft.com/en-us/windows-hardware/drivers/usbcon/usb-driver-installation-based-on-compatible-ids#configure-selective-suspend-for-usbsersys
+)
+
+$regPaths = Get-ChildItem 'HKLM:\SYSTEM\ControlSet001\Enum\USB' -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.Name -like '*Device Parameters' } 
+
+foreach ($regPath in $regPaths) {
+    $props = Get-ItemProperty $regPath.PSPath | Get-Member -MemberType NoteProperty
+    foreach ($value in $possibleValues) {
+        if (@($props.Name) -contains $value.Name) {
+            if ($value.Name -eq 'IdleUsbSelectiveSuspendPolicy') {
+                Remove-ItemProperty -Path $regPath.PSPath -Name $value.Name -Force 
+            }
+            else {
+                New-ItemProperty -Path $regPath.PSPath -Name $value.Name -Value $value.Value -PropertyType $value.Type -Force
+            }
+        }
+    }
+    #enable device manager checkbox for all
+    New-ItemProperty -Path $regPath.PSPath -Name 'UserSetDeviceIdleEnabled' -Value 0 -PropertyType 'Dword' -Force
+}
